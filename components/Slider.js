31ff882c60cd208b11a2b657f3e7c62d3d19e338
Florencia Temporelli <fltemporelli@gmail.com>
2021-11-22 16:10:43 -0300

    I finally made the slider work with hooks

--- components/Slider.js
+++ components/Slider.js
@@ -1,10 +1,28 @@
+import styles from './Slider.module.css'
+import React, { useState, useEffect } from 'react';
+
 const Slider = ({ facts }) => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => { 
+      if (count < 20) {
+        setCount((c) => c + 1) 
+      }
+      else {
+        setCount(0);
+      }
+    }, 1000);
+  
+    return () => {
+      clearInterval(interval);
+    }  
+  })
+
   return (
-    <ul>
-      {facts.map((fact, i) => (
-        <li key={i}>{fact.fact}</li>
-      ))}
-    </ul>
+    <div className={styles.slide}>
+      {facts[count].fact}
+    </div>
   )
 }
 
